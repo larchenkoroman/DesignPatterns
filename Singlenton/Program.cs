acbba6c951265ab4665142449657a8253830385f
larchenkoroman <larchenkoroman@gmail.com>
2025-01-25 22:13:49 +0200

    Singlenton

---
+++ Singlenton/Program.cs
@@ -0,0 +1,14 @@
+﻿Singlenton singlenton1 = Singlenton.Instance;
+Singlenton singlenton2 = Singlenton.Instance;
+Singlenton singlenton3 = Singlenton.Instance;
+
+sealed class Singlenton
+{
+    private static readonly Lazy<Singlenton> _lazyInstance = new(() => new Singlenton());
+
+    public static Singlenton Instance => _lazyInstance.Value;
+    private Singlenton() 
+    {
+        Console.WriteLine("Instaciating Singlenton");
+    }
+}
