5a755ea6cfc6e48f91b58672ef7ef60d11663d0d
larchenkoroman <larchenkoroman@gmail.com>
2025-01-25 22:36:04 +0200

    SinglentonDI

---
+++ SinglentonDI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Channels;
+
+IServiceCollection services =  new ServiceCollection();
+
+services.AddSingleton<Logger>();
+var serviceProvider = services.BuildServiceProvider();
+
+var logger1 = serviceProvider.GetRequiredService<Logger>();
+var logger2 = serviceProvider.GetRequiredService<Logger>();
+var logger3 = serviceProvider.GetRequiredService<Logger>();
+var logger4 = serviceProvider.GetRequiredService<Logger>();
+
+Console.WriteLine(logger1.Equals(logger2).ToString());
+
+class Logger
+{
+    public Logger()
+    {
+        Console.WriteLine("Loger instanciating");
+    }
+}
